f8cb412a76e8c3e8ebaf2a1b3ac204ed8b63f39c
Marcus <marcuspanozzo@gmail.com>
2020-09-23 03:34:20 -0400

    Added handling for binding sequentially to multiple sockets. Error handling still needed

--- Assignment/Assignment1/main.c
+++ Assignment/Assignment1/main.c
@@ -20,7 +20,7 @@
 #include <stdbool.h>
 #include <inttypes.h>
 
-
+#define DEBUG  1
 
 // Operation codes (opcode)
 #define OP_RRQ       0
@@ -28,6 +28,11 @@
 #define OP_DATA      2
 #define OP_ACK       3
 #define OP_ERR       4
+
+
+
+
+
 
 // Packet decription structures
 // Placed inside a union for convenience
@@ -79,9 +84,19 @@ typedef union
 
 tftp_packet format_packet(int opcode)
 {
-   
+
 }
 
+
+// SIGNAL handlers
+void sigchld_handler(int v)
+{
+   pid_t pidC;   
+   pidC = wait(NULL); // Block parent until child completes
+  
+   printf("[Parent] Parent confirms child (pid=%d) finished.\n",pidC);
+   
+}
 
 // main(): Starting point for TFTP server. 
 // Designed to handle two additional arguments (argc=3)
@@ -92,14 +107,110 @@ tftp_packet format_packet(int opcode)
 // while executing
 int main(int argc, char** argv)
 {
-   // Set up SIGNAL handlers for when clients connect
-   // or if there are SIGNAL errors 
-
-   // Set up server socket
-   struct sockaddr_in sock_desc; // Stores information about the socket
-   int socket_fd; // Socket file descriptor
-   socklen_t sd_len = sizeof(sock_desc); // Total bytes length of the socket description
-
+   // Do a sanity check to make sure we have enough arguments
+   if (argc < 3)
+   {
+      perror("[Error] Invalid number of arguments!\n
+      Args: port_start_range port_end_range\n");
+      exit(EXIT_FAILURE);
+   }
+   // Parse the range borders (inclusive)
+   int portCounter = rangeStart;
+   int rangeStart = atoi(argv[1]);
+   int rangeEnd = atoi(argv[2]);
    
 
+   // Set up SIGNAL handlers for when clients connect
+   // or if there are SIGNAL errors 
+   signal(SIGCHLD,sigchld_handler);
+   
+   // Set up server socket
+   struct sockaddr_in servaddr; // Stores information about the socket
+   int socket_fd; // Socket file descriptor
+   socklen_t addrlen = sizeof(servaddr); // Total bytes length of the socket description
+   memset(&sock_desc,0,addrlen); // Set the socket description to blank
+
+   // tftp server setup using udp protocol
+   
+   socket_fd = socket(PF_INET,SOCK_DGRAM,0);
+   if (socket_fd < 0)
+   {
+      // Error: Socket file descriptor could not be initialized
+      perror("[Error] socket file descriptor failure!\n");
+      exit(EXIT_FAILURE);
+   }
+
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_addr.s_addr = INADDR_ANY;
+
+   // Essential variables
+   tftp_packet myPacket;
+   uint16_t ret;
+   unsigned short int opc; // operational code
+   uint16_t* opc_ptr; // pointer for operational code
+   while(portCounter <= rangeEnd)
+   {
+      // Set server to listen on specific port in the range
+      servaddr.sin_port = htons(port_cntr);
+      if (bind(socket_fd,(struct sockaddr *)&servaddr,addrlen) < 0)
+      {
+         // Binding failure!
+         perror("[Error] Binding failure!\n");
+         exit(EXIT_FAILURE);
+      }
+      // Confirm the socket port
+      struct sockaddr_in clientaddr;
+      socklen_t slen = sizeof(clientaddr);
+      getsockname(socket_fd,(struct sockaddr *)&servaddr,&addrlen);
+      printf("[Server] Bound on port %d\n",ntohs(servaddr.sin_port));
+      ret = recvfrom(socket_fd,&myPacket,sizeof(myPacket),0,
+               (struct sockaddr *)&clientaddr,&slen);
+
+      if (ret < 0)
+      {
+         if (errno == EINTR)
+         {
+            continue; // TODO: saw this but no idea
+         }
+
+         // Error on packet?
+         perror("[Error] recvfrom() failure! Could not receive from client\n");
+         exit(EXIT_FAILURE);
+      }
+      // --------------
+      // Packet received! If we have a rrq or wrq, fork
+      // off process and conduct the transfer of data
+
+      //  Extract the opcode from the received initial packet
+      uint16_t opcTemp;  // Used to get the opcode at this current iteration
+      opc_ptr = &opcTemp;
+      *opc_ptr = myPacket.opcode;
+      opc = ntohs(*opc_ptr); // Convert value to perm storage across loops
+      printf("[debug] received packet with opcode %d\n",opc);
+      if (opc == OP_RRQ || opc == OP_WRQ)
+      {
+         // We have a read or write request
+         // use the appropriate handling
+         pid_t pval = fork();
+         if (pval == 0)
+         {
+            // We are a child process
+            if (opc == OP_RRQ)
+            {
+               // TODO: insert RRQ call here
+            }
+            if (opc == OP_WRQ)
+            {
+               // TODO: insert WRQ call here
+            }
+         } else {
+            // We are a parent process
+            // Nothing more to do
+         }
+      } else {
+
+      }    
+      port_cntr += 1; // Increment port counter
+   }
+   return 0;
 }
